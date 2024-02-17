26a342f22b25f46554d5c7edf3ee5a695c1c5148
DamlaDay <damladayal@gmail.com>
2024-02-17 12:42:34 -0500

    I have work on planet2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -12,7 +12,10 @@ public class SolarSystem {
         xros.temp="23232323232323k";
         xros.Size="545454545455miles";
 
-
+        planet2 gecko =new planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";
     }
 
 }
