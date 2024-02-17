c21f535caa3a346e78fe180da61b2b445a4c52cb
DamlaDay <damladayal@gmail.com>
2024-02-17 12:26:54 -0500

    I have worked on planet1

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -12,6 +12,10 @@ public class SolarSystem {
         xros.temp="23232323232323k";
         xros.Size="545454545455miles";
 
+Planet1 frog=new Planet1();
+frog.temp="5";
+frog.color="blue";
+frog.size="2km";
 
     }
 
